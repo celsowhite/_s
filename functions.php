94f55bdaa52d690f6a905a86b89be8bd769ca9d3
celsowhite <celso.white13@gmail.com>
2015-08-20 11:19:33 -0400

    First commit. Update of functions.php with general settings and  enqueues

--- functions.php
+++ functions.php
@@ -110,11 +110,42 @@ function _s_widgets_init() {
 }
 add_action( 'widgets_init', '_s_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
+/*==========================================
+ENQUEUE SCRIPTS AND STYLES
+==========================================*/
+
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+
+	/*=== Minified Foundation 5 ===*/
+
+	wp_enqueue_style( 'foundation', get_template_directory_uri() . '/css/foundation/foundation.min.css' );
+
+	wp_enqueue_style( 'foundation_normalize', get_template_directory_uri() . '/css/foundation/normalize.css' );
+
+	/*=== Font Awesome ===*/
+
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+
+	/*=== Compiled SCSS File ===*/
+
+	wp_enqueue_style( 'custom_styles', get_template_directory_uri() . '/css/style.css' );
+
+	/*=== Flexslider Styles ===*/
+
+	wp_enqueue_style('flexslider', get_template_directory_uri() . '/css/flexslider/flexslider.css');
+
+	wp_enqueue_script( '_s-modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.custom.js', '','', true );
+
+	wp_enqueue_script( '_s-classie', get_template_directory_uri() . '/js/classie/classie.js', '','', true );
+
+	/*=== Custom Scripts ===*/
+
+	wp_enqueue_script('_s-scripts', get_template_directory_uri() . '/js/scripts.js', '', '', true);
+
+	/*=== Flexslider JS ===*/
+
+	wp_enqueue_script( '_s-flexslider', get_template_directory_uri() . '/js/flexslider/jquery.flexslider-min.js', '','', true);
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
@@ -123,8 +154,122 @@ function _s_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	/*=== Wordpress Default Jquery ===*/
+
+	if (!is_admin()) {
+		wp_enqueue_script('jquery');
+	}
+
 }
+
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/*====================================================================
+
+Enqueue Typekit if Neccesary
+
+function theme_typekit() {
+    wp_enqueue_script( 'theme_typekit', '//use.typekit.net/mjs0clp.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+
+function theme_typekit_inline() {
+  if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+  	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<?php }
+}
+add_action( 'wp_head', 'theme_typekit_inline' );
+
+====================================================================*/
+
+/*==========================================
+CUSTOM POST TYPES
+==========================================*/
+
+/*=== Stories ===*/
+
+function custom_post_type_stories() {
+
+	$labels = array(
+		'name'                => _x( 'Stories', 'Post Type General Name'),
+		'singular_name'       => _x( 'Stories', 'Post Type Singular Name'),
+		'menu_name'           => __( 'Stories'),
+		'parent_item_colon'   => __( ''),
+		'all_items'           => __( 'All Stories'),
+		'view_item'           => __( 'View Story'),
+		'add_new_item'        => __( 'Add New Story'),
+		'add_new'             => __( 'Add New'),
+		'edit_item'           => __( 'Edit Story'),
+		'update_item'         => __( 'Update Story'),
+		'search_items'        => __( 'Search Stories'),
+		'not_found'           => __( 'Not Found'),
+		'not_found_in_trash'  => __( 'Not found in Trash'),
+	);
+	
+	$args = array(
+		'label'               => __( 'stories'),
+		'description'         => __( 'Stories'),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'page-attributes', 'custom-fields', 'excerpt'  ),
+		'hierarchical'        => true,
+		'taxonomies'          => array( 'story_category' ),
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+	);
+
+	register_post_type( 'stories', $args );
+
+}
+
+add_action( 'init', 'custom_post_type_stories', 0 );
+
+/*==========================================
+CUSTOM TAXONOMY 
+==========================================*/
+
+/*=== Stories Taxonomy ===*/
+
+function add_stories_taxonomies() {
+
+  register_taxonomy('type', 'stories', array(
+
+      'hierarchical' => true,
+      'labels' => array(
+      'name' => _x( 'Type', 'taxonomy general name' ),
+      'singular_name' => _x( 'Type', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Types' ),
+      'all_items' => __( 'All Types' ),
+      'parent_item' => __( 'Parent Type' ),
+      'parent_item_colon' => __( 'Parent Type:' ),
+      'edit_item' => __( 'Edit Type' ),
+      'update_item' => __( 'Update Type' ),
+      'add_new_item' => __( 'Add New Type' ),
+      'new_item_name' => __( 'New Type Name' ),
+      'menu_name' => __( 'Types' ),
+    ),
+
+      'rewrite' => array(
+      'slug' => 'type', 
+      'with_front' => false,
+      'hierarchical' => false
+    ),
+  ));
+}
+add_action( 'init', 'add_stories_taxonomies', 0 );
+
+/*==========================================
+INCLUDES
+==========================================*/
 
 /**
  * Implement the Custom Header feature.
