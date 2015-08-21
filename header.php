74ad7203e045a93c8b55be1af61c4cf6bf556eb0
celsowhite <celso.white13@gmail.com>
2015-08-21 10:19:40 -0400

    Add google analytics to header.

--- header.php
+++ header.php
@@ -17,6 +17,24 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!-- Google Analytics Pulled from Options Page Created by ACF -->
+
+<?php if(get_field('options_google_analytics','option')): ?>
+
+	<script>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '<?php the_field('options_google_analytics','option'); ?>']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+<?php endif; ?>
+
 <?php wp_head(); ?>
 </head>
 
