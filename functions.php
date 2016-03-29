a2278df16edf03cfb8b363fb47a47d7e06c272da
celsowhite <celso.white13@gmail.com>
2016-03-29 18:12:24 -0400

    Add script to remove Wordpress default emoji styles and scripts.

--- functions.php
+++ functions.php
@@ -96,6 +96,15 @@ function _s_content_width() {
 }
 add_action( 'after_setup_theme', '_s_content_width', 0 );
 
+/*==========================================
+REMOVE WP EMOJI
+==========================================*/
+
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 /*==========================================
 REGISTER WIDGET AREA
