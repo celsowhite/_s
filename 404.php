581242ebf7849cc77fda987f2ba199258b480150
celsowhite <celso.white13@gmail.com>
2016-07-08 08:24:41 -0400

    Clean up 404 page so it doesn't include the loop.

--- 404.php
+++ 404.php
@@ -11,23 +11,19 @@ get_header(); ?>
 
 	<main id="main" class="main_wrapper" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<header class="page_header">
+			<?php the_title(); ?>
+		</header>
 
-			<header class="page_header">
-				<?php the_title(); ?>
-			</header>
+		<div class="page_content">
+			<?php the_content(); ?>
+		</div>
 
-			<div class="page_content">
-				<?php the_content(); ?>
-			</div>
-
-			<?php
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; ?>
+		<?php
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+		?>
 
 	</main>
 
