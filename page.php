0dde68a0d0201b2458d8f5fb88dfacf6db1e8246
celsowhite <celso.white13@gmail.com>
2018-09-09 22:34:19 -0400

    Add default content to default page template.

--- page.php
+++ page.php
@@ -10,11 +10,37 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<header class="page_header">
-				<?php the_title(); ?>
+				<div class="container">
+					<h1><?php the_title(); ?></h1>
+				</div>
 			</header>
 
-			<div class="page_content wysiwyg">
-				<?php the_content(); ?>
+			<div class="page_content">
+				<div class="container">
+					<div class="wysiwyg">
+						<?php the_content(); ?>
+					</div>
+					<div class="panel">
+						<iframe src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" allowfullscreen allowtransparency allow="autoplay"></iframe>
+					</div>
+					<div class="panel">
+						<div class="plyr__video-embed" id="player">
+							<iframe src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" allowfullscreen allowtransparency allow="autoplay"></iframe>
+						</div>
+					</div>
+					<div class="panel">
+						<?php get_template_part('template-parts/components', 'social_share'); ?>
+					</div>
+					<div class="panel">
+						<div class="flexslider">
+							<ul class="slides">
+								<li><img src="https://celsowhite.com/static/img/websites/rownewyork/featured_image_large.jpg" /></li>
+								<li><img src="https://celsowhite.com/static/img/websites/jimmychin/featured_image_large.jpg" /></li>
+								<li><img src="https://celsowhite.com/static/img/websites/latinousa/featured_image_large.jpg" /></li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
 
 		<?php endwhile; ?>
